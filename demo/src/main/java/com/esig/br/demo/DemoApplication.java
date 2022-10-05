cdc76e237c27928dc46f40294e9c26643186b081
Paulo César <o.paulo.c@gmail.com>
2022-10-05 16:40:12 -0300

    Bean para preencher database inicial enves do sqlscript

--- demo/src/main/java/com/esig/br/demo/DemoApplication.java
+++ demo/src/main/java/com/esig/br/demo/DemoApplication.java
@@ -1,13 +1,35 @@
 package com.esig.br.demo;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.esig.br.demo.model.Tarefa;
+import com.esig.br.demo.repository.TarefaRepository;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init (TarefaRepository tarefaRepository){
+		return args -> {
+			List<Tarefa> tarefas = Arrays.asList(
+				new Tarefa(null, "Tarefa01", "Uma descrição da tarefa01", "Paulo", "1", new Date()),
+				new Tarefa(null, "Tarefa02", "Uma descrição da tarefa02", "Paulo", "1", new Date()),
+				new Tarefa(null, "Tarefa03", "Uma descrição da tarefa03", "Paulo", "1", new Date())
+				);
+
+			tarefas.forEach(tarefaRepository::save);
+		};
 	}
 
 }
