233aad69abde497caf635398ff2bbf6a27e7dcba
Paulo César <o.paulo.c@gmail.com>
2022-10-06 13:41:37 -0300

    Controller para a classe 'Responsavel'

---
+++ demo/src/main/java/com/esig/br/demo/controller/ResponsavelController.java
@@ -0,0 +1,46 @@
+package com.esig.br.demo.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.esig.br.demo.domain.model.Responsavel;
+import com.esig.br.demo.repository.ResponsavelRepository;
+
+@Component
+@ViewScoped
+public class ResponsavelController implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Autowired
+    transient ResponsavelRepository responsavelRepository;
+
+    private transient Map<String,Responsavel> responsaveisMap = new HashMap<>();
+
+    @PostConstruct
+    public void loadData() {
+        List<Responsavel> responsaveis = responsavelRepository.findAll();
+        responsaveis.forEach(r -> responsaveisMap.put(r.getNome(), r));
+    }
+
+    public Responsavel saveOrUpdateAndFlush(Responsavel responsavel){
+        if(!responsaveisMap.containsKey(responsavel.getNome())) {
+            responsavel = responsavelRepository.saveAndFlush(responsavel);
+            responsaveisMap.put(responsavel.getNome(), responsavel);
+            return responsavel;
+        }
+        return responsaveisMap.get(responsavel.getNome());
+    }
+
+    public List<Responsavel> getResponsaveis(){
+        return new ArrayList<>(responsaveisMap.values());
+    }
+}
