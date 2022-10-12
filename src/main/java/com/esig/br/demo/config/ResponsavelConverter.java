c897ee0d99ab83fd3e5ae60d4c69db0af8eefc41
Paulo César <o.paulo.c@gmail.com>
2022-10-12 14:14:45 -0300

    bugfix do campo de buscar por resposavel não editavel

--- src/main/java/com/esig/br/demo/config/ResponsavelConverter.java
+++ src/main/java/com/esig/br/demo/config/ResponsavelConverter.java
@@ -1,5 +1,8 @@
 package com.esig.br.demo.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
@@ -10,14 +13,20 @@ import com.esig.br.demo.domain.model.Responsavel;
 @FacesConverter(forClass=Responsavel.class)
 public class ResponsavelConverter implements Converter<Object>{
 
+    private static Map<String, Responsavel> responsaveis = new HashMap<>(); 
+
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        return new Responsavel(null, value);
+        return responsaveis.getOrDefault(value, new Responsavel(null, value));
     }
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
         Responsavel responsavel = (Responsavel) value;
+        if(responsaveis.containsValue(responsavel)){
+            return responsavel.getNome();
+        }
+        responsaveis.put(responsavel.getNome(), responsavel);
         return responsavel.getNome();
     }
 }
